f1f851a4ee5c18310128d7f1dd4effa01d2a936a
Sinitsa <sinitsammi@gmail.com>
2024-12-02 19:50:47 +0300

    firstTask

---
+++ MasteringCycles/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace MasteringCycles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
+            string message;
+            int repeats;
+
+            Console.Write("Введите сообщение: ");
+            message = Console.ReadLine();
+            Console.Write("Введите количество повторений: ");
+            repeats = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < repeats; i++)
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
