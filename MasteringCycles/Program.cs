f461c5f674fc88356eb7437993e2adf2879efe06
SinitsaEV <sinitsammi@gmail.com>
2024-12-03 17:46:45 +0300

    Update Program.cs

--- MasteringCycles/Program.cs
+++ MasteringCycles/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Text;
 
 namespace MasteringCycles
@@ -11,18 +11,45 @@ namespace MasteringCycles
 
             int randomNumber = random.Next();
             int degreeOfNumber = 2;
-            int minNumber = 1;
+            int desiredNumber = 1;
             int minDegree = 0;
 
             Console.OutputEncoding = Encoding.Unicode;
 
-            while (minNumber < randomNumber )
+            while (desiredNumber <= randomNumber)
             {
                 minDegree++;
-                minNumber *= degreeOfNumber;
+                desiredNumber *= degreeOfNumber;
             }
-            
-            Console.WriteLine($" случайное число: {randomNumber}, минимальная степень {minDegree}, число в этой степени {minNumber}");
+
+            Console.WriteLine($" случайное число: {randomNumber}, минимальная степень {minDegree}, число в этой степени {desiredNumber}");
+        }
+    }
+}using System;
+using System.Text;
+
+namespace MasteringCycles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int randomNumber = random.Next();
+            int degreeOfNumber = 2;
+            int desiredNumber = 1;
+            int minDegree = 0;
+
+            Console.OutputEncoding = Encoding.Unicode;
+
+            while (desiredNumber <= randomNumber)
+            {
+                minDegree++;
+                desiredNumber *= degreeOfNumber;
+            }
+
+            Console.WriteLine($" случайное число: {randomNumber}, минимальная степень {minDegree}, число в этой степени {desiredNumber}");
         }
     }
 }
